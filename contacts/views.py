8de89da2bcf064b3da7dfdcc9af0dfb7019eb4c0
Wes Mitchell <wes.mitchell@gmail.com>
2022-02-20 21:52:34 -0500

    updated views.py for correct layout

--- contacts/views.py
+++ contacts/views.py
@@ -23,3 +23,24 @@ class Window(QMainWindow):
         self.setCentralWidget(self.centralWidget)
         self.layout = QHBoxLayout()
         self.centralWidget.setLayout(self.layout)
+        self.setupUI()
+
+    def setupUI(self):
+        """Setup the main window's GUI"""
+        # Create the table view widget
+        self.table = QTableView()
+        # self.table.setSelectionBehavior(QAbstractItemView.selectionBehavior)
+        self.table.resizeColumnsToContents()
+        # Create buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+        #Layout the GUI
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addWidget(self.clearAllButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
